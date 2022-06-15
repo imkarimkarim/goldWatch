64420f6d4cc7537a728a4e4cda55c733f4a1e9f8
imkarimkarim <allalirezamail@gmail.com>
2022-06-15 18:12:34 +0430

    working with DB + notif when statement is true

--- src/app.js
+++ src/app.js
@@ -1,25 +1,46 @@
 "use strict";
 
-import { extractfPrice } from "./utils.js";
+// import { extractfPrice } from "./utils.js";
 import puppeteer from "puppeteer";
+import DB from "./db.js";
+import { isStatementTrue } from "./statmentChecker.js";
+import { extractfPrice } from "./utils.js";
 
-(async () => {
+const db = new DB();
+
+db.getSent(async (sent) => {
   const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  console.log("goint to tsetmc.com!");
-  const url =
-    "http://tsetmc.com/Loader.aspx?ParTree=151311&i=46700660505281786";
-  await page.goto(url);
-  console.log("getting the price in 10 second...");
-  await page.waitForTimeout(10000);
-  const searchValue = await page.$eval("#d02", (el) => el.innerHTML);
-  console.log("current price: ", extractfPrice(searchValue));
-
-  // await browser.waitForTarget(
-  //   () => {
-  //     return false;
-  //   },
-  //   { timeout: 0 }
-  // );
-  await browser.close();
-})();
+  if (!sent) {
+    const loop = setInterval(async () => {
+      db.getSent(async (sent) => {
+        if (!sent) {
+          const page = await browser.newPage();
+          console.log("goint to tsetmc.com!");
+          const url =
+            "http://tsetmc.com/Loader.aspx?ParTree=151311&i=46700660505281786";
+          await page.goto(url);
+          console.log("getting the price in 10 second...");
+          await page.waitForTimeout(10000);
+          const html = await page.$eval("#d02", (el) => el.innerHTML);
+          console.log("current price: ", extractfPrice(html));
+          const price = extractfPrice(html);
+          isStatementTrue(price, (bool) => {
+            if (bool) {
+              console.log("notif!");
+            } else {
+              console.log("not yet...");
+            }
+          });
+        } else {
+          db.setSent(false);
+          await browser.close();
+          clearInterval(loop);
+        }
+      });
+    }, 1000 * 20);
+  } else {
+    db.setSent(false);
+    await browser.close();
+    clearInterval(loop);
+  }
+});
