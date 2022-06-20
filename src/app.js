de11df7989a4c20e365afd7685d1a4ccc6123a54
imkarimkarim <allalirezamail@gmail.com>
2022-06-20 21:03:10 +0430

    reading price from the same page instead of opening new one every 20second

--- src/app.js
+++ src/app.js
@@ -7,23 +7,22 @@ import { isStatementTrue } from "./statmentChecker.js";
 import { extractfPrice } from "./utils.js";
 
 const db = new DB();
-const browser = await puppeteer.launch({ headless: true });
+const browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+
+console.log("goint to tsetmc.com!");
+const url = "http://tsetmc.com/Loader.aspx?ParTree=151311&i=46700660505281786";
+await page.goto(url);
+console.log("getting the price in 10 second...");
+await page.waitForTimeout(10000);
 
 const loop = setInterval(async () => {
   db.getSent(async (sent) => {
     if (!sent) {
-      const page = await browser.newPage();
-      console.log("goint to tsetmc.com!");
-      const url =
-        "http://tsetmc.com/Loader.aspx?ParTree=151311&i=46700660505281786";
-
-      await page.goto(url);
-      console.log("getting the price in 10 second...");
-      await page.waitForTimeout(10000);
       const html = await page.$eval("#d02", (el) => el.innerHTML);
-
       console.log("current price: ", extractfPrice(html));
       const price = extractfPrice(html);
+
       isStatementTrue(price, (bool) => {
         if (bool) {
           console.log("notif!");
