ef44efef82bfe363e8f0cfc6a4fc58710b7c4519
imkarimkarim <allalirezamail@gmail.com>
2022-09-04 16:30:51 +0430

    redis config

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,5 +1,10 @@
 import Redis from "ioredis";
+import "dotenv/config";
 
-const redis = new Redis();
+const redis = new Redis({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASS,
+});
 
 export default redis;
