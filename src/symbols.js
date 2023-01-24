ac813dd01029afb8291df1e8831f07da3afc2f86
imkarimkarim <imkarimkarim@pm.me>
2023-01-24 13:19:35 +0330

    revert!: switching to cmd version
    removing this version which is a node+vanilla js client version
    and switching to a cmd version which will only search for gold symbol

--- src/symbols.js
+++
@@ -1,6 +0,0 @@
-const url = "http://tsetmc.com/tsev2/data/instinfodata.aspx?i=";
-
-export const symbols = {
-  طلا: url + "46700660505281786&c=68%20&e=1",
-  سکه: url + "16255851958781005&c=56%20",
-};
